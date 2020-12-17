3f39756a921c7e61af03c81dc42a3c0584d0d935
Timofey Kuznetsov <tima1702@gmail.com>
2020-12-17 11:40:07 +0300

    222222

--- model/Client.js
+++ model/Client.js
@@ -1,20 +1,33 @@
 const { DataTypes, Model } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize) => {
-    class Client extends Model {
+  class Client extends Model {
+    comparePassword(hash) {
+      return bcrypt.compareSync(this.password, hash);
     }
-    Client.init({
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        phone: {
-            type: DataTypes.STRING,
-        },
-    }, {
-        sequelize,
-    });
+  }
 
-    return Client;
+  Client.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+  });
+
+  Client.beforeCreate(async (user) => {
+    const hashPass = await bcrypt.hashSync(user.password, 10);
+    user.password = hashPass;
+  });
+  return Client;
 };
